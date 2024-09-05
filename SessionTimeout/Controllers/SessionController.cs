8d45180cf6ee824db9cdf07ab89396e81b4332d0
Arjun Pawar <Arjun.Pawar@aurionpro.com>
2024-09-05 23:09:30 +0530

    Add project files.

---
+++ SessionTimeout/Controllers/SessionController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SessionTimeout.Controllers
+{
+    public class SessionController : Controller
+    {
+        // GET: Session
+        public ActionResult Index()
+        {
+            Session["User"] = "John Doe";
+            Session["LastAccessTime"] = DateTime.Now;
+            return View();
+        }
+        public ActionResult GetSession()
+        {
+            // Retrieve session values
+            string user = Session["User"] as string;
+            DateTime? lastAccessTime = Session["LastAccessTime"] as DateTime?;
+
+            if (user != null && lastAccessTime != null)
+            {
+                ViewBag.Message = $"User: {user}, Last Access Time: {lastAccessTime}";
+            }
+            else
+            {
+                ViewBag.Message = "Session has expired or not set.";
+            }
+
+            return View();
+        }
+
+        public ActionResult CheckSessionTimeout()
+        {
+            // Check if session exists
+            if (Session["User"] == null)
+            {
+                return RedirectToAction("SessionExpired");
+            }
+
+            // Update session last access time
+            Session["LastAccessTime"] = DateTime.Now;
+
+            return View();
+        }
+
+        public ActionResult SessionExpired()
+        {
+            return View();
+        }
+    }
+}
